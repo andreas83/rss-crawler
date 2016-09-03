90772423e6e03829b71a5599ee4e07f691896504
Andreas Beder <office@moving-bytes.at>
2016-09-04 00:26:17 +0200

    Update rssitemparser.py
    added language detection
    added rss resources (make sure to update rssly api model first)

--- rssitemparser.py
+++ rssitemparser.py
@@ -3,12 +3,13 @@ import scrapy
 import csv
 import psycopg2
 import feedparser
+import datetime
 from langdetect import detect
-
+import pprint
 
 
 def get_urls_from_csv():
-    with open('/var/www/is.codejungle.org/rss/results/res.csv', newline='') as csv_file:
+    with open('res.csv', newline='') as csv_file:
         data = csv.reader(csv_file, delimiter=',')
         scrapurls = []
         for row in data:
@@ -37,29 +38,43 @@ class rssitemparser(scrapy.Spider):
     def parse(self, response):
         f = feedparser.parse(response.body)
         cursor = self.conn.cursor()
-        sql_string = "INSERT INTO api_rsssource (name,description,image,link, language, created_date, modified_date) VALUES (%s,%s,%s,%s,%s, now(), now()) RETURNING id"
-        img="";
-        if hasattr(f.feed, "image"):
-            img=f.feed.image
+        sql_string = "INSERT INTO api_rsssource (name,description,image,link, language, created_date, modified_date) VALUES (%s,%s,%s,%s,%s, now(), %s) RETURNING id"
 
-        lang=detect(f.feed.description)
+
+        title=f.feed.get("title", "")
+        img=f.feed.get("image", "")
+        desc=f.feed.get("description", "");
+        link=f.feed.get("link", "");
+        pub=f.feed.get("published", datetime.datetime.now())
+        lang=detect(title)
+
+        if(isinstance(img, feedparser.FeedParserDict)):
+            img=img.href
+
 
         try:
-            cursor.execute(sql_string, (f.feed.title, f.feed.description, img, f.feed.link, lang ))
+            cursor.execute(sql_string, (title, desc, img, link, lang, pub))
         except Exception as e:
             print(e)
             pass
         self.conn.commit()
         nid = cursor.fetchone()[0]
-        print(nid)
+
         for item in f.entries:
-            sql_string = 'INSERT INTO api_rssitem (title, description, author, link, "RSSSource_id", created_date, modified_date) VALUES (%s,%s,%s,%s,%s,now(), now())';
-            author="";
-            if hasattr(item, "author"):
-                author=item.author
+            sql_string = 'INSERT INTO api_rssitem (title, description, author, link, "RSSSource_id", created_date, modified_date) VALUES (%s,%s,%s,%s,%s,now(), %s) RETURNING id';
+            author=item.get("author", "")
+            desc=item.get("description", "")
+            pub=item.get("pubDate", datetime.datetime.now())
+            enclosures=item.get("enclosures", 0)
+
             try:
-                cursor.execute(sql_string, (item.title, item.description, author, item.link, nid))
+                cursor.execute(sql_string, (item.title, desc, author, item.link, nid, pub))
             except Exception as e:
                 print(e)
                 pass
-        self.conn.commit()
+            self.conn.commit()
+            itemid=cursor.fetchone()[0]
+
+            for img in enclosures:
+                sql='INSERT INTO api_rssitemresource (link, mime, "RSSItem_id", created_date, modified_date) VALUES (%s, %s,%s,now(),now())'
+                cursor.execute(sql, (img.href, img.type, itemid))
